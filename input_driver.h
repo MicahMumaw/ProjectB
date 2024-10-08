fc26ad27c76616817199561c9a1ef3ae65de7b7a
EvanEggering <eeggering@gmail.com>
2024-10-08 14:20:22 -0400

    Writing methods for input driver
    So much because I couldn't get my changes to show in github desktop Micah found my error wasn't saving in folder with Git

--- input_driver.h
+++ input_driver.h
@@ -5,11 +5,29 @@
  *      Author: eegge
  */
 
-#ifndef GIT_PROJECTB_INPUT_DRIVER_H_
-#define GIT_PROJECTB_INPUT_DRIVER_H_
+#include "stm32l4xx_hal.h"
+
+class InputDriver
+{
+private:
+    Queue* queue;
+    int counter;
+    Semaphore* semaphore;
+    int pinA;
+    int pinB;
+    int lastState;
 
 
+public:
 
+    InputDriver(Queue* q, Semaphore* sem, int pinA, int pinB);
 
+    void update();
+
+    void incrementCounter();
+    void decrementCounter();
+
+    void getCounter(int *counterValue);
+};
 
 #endif /* GIT_PROJECTB_INPUT_DRIVER_H_ */

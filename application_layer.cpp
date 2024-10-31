2c416f5b6c376a24077bd9af17a6337de1211a64
MicahMumaw <micahandrew96@gmail.com>
2024-10-31 17:50:46 -0400

    redo previous commit

--- application_layer.cpp
+++ application_layer.cpp
@@ -1,85 +1,85 @@
-///*
-// * application_layer.cpp
-// *
-// *  Created on: Sep 24, 2024
-// *      Author: micah
-// */
-//#include <cstdint>
-//#include <cmath>
-//
-//#include "application_layer.h"
-//
-//SignalSynthesis::SignalSynthesis() : shape(SINE), freq(1000.0f), amp(1.0f), followerMode(false), delay(0) {};
-//
-//
-//enum SignalSynthesis::Shape {SINE, SQUARE, PULSE10};
-//
-//
-//void SignalSynthesis::setShape(Shape shape) {
-//	this -> shape = shape;
-//	return;
-//}
-//
-//
-//void SignalSynthesis::setFrequency(float freq) {
-//	if(freq >= 1.0f && freq <= 1000.0f){
-//		this -> freq = freq;
-//	}
-//	return;
-//}
-//
-//
-//void SignalSynthesis::setAmplitude(float amp) {
-//	this -> amp = amp;
-//	return;
-//}
-//
-//
-//void SignalSynthesis::update() {
-//	return;
-//}
-//
-//
-//void SignalSynthesis::enableFollowerMode(bool enable) {
-//	followerMode = enable;
-//	return;
-//}
-//
-//
-//void SignalSynthesis::setDelay(int step) {
-//	if(step >= 0 && step <= 7) {
-//		delay = step * (1.0f / freq) / 8;
-//	}
-//	return;
-//}
-//
-//
-//Semaphore::Semaphore(int count = 0) : count(count) {};
-//
-//
-//void Semaphore::post() {
-//	count++;
-//	return;
-//}
-//
-//
-//void Semaphore::wait() {
-//	while(count <= 0) {}
-//	return;
-//}
-//
-//
-//bool Semaphore::tryWait() {
-//	if(count > 0) {
-//		count--;
-//		return true;
-//	}
-//	else {
-//		return false;
-//	}
-//}
-//
-//
-//int Sempahore::getCount() {
-//	return count;
-//}
+/*
+ * application_layer.cpp
+ *
+ *  Created on: Sep 24, 2024
+ *      Author: micah
+ */
+#include <cstdint>
+#include <cmath>
+
+#include "application_layer.h"
+
+SignalSynthesis::SignalSynthesis() : shape(SINE), freq(1000.0f), amp(1.0f), followerMode(false), delay(0) {};
+
+
+enum SignalSynthesis::Shape {SINE, SQUARE, PULSE10};
+
+
+void SignalSynthesis::setShape(Shape shape) {
+	this -> shape = shape;
+	return;
+}
+
+
+void SignalSynthesis::setFrequency(float freq) {
+	if(freq >= 1.0f && freq <= 1000.0f){
+		this -> freq = freq;
+	}
+	return;
+}
+
+
+void SignalSynthesis::setAmplitude(float amp) {
+	this -> amp = amp;
+	return;
+}
+
+
+void SignalSynthesis::update() {
+	return;
+}
+
+
+void SignalSynthesis::enableFollowerMode(bool enable) {
+	followerMode = enable;
+	return;
+}
+
+
+void SignalSynthesis::setDelay(int step) {
+	if(step >= 0 && step <= 7) {
+		delay = step * (1.0f / freq) / 8;
+	}
+	return;
+}
+
+
+Semaphore::Semaphore(int count = 0) : count(count) {};
+
+
+void Semaphore::post() {
+	count++;
+	return;
+}
+
+
+void Semaphore::wait() {
+	while(count <= 0) {}
+	return;
+}
+
+
+bool Semaphore::tryWait() {
+	if(count > 0) {
+		count--;
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
+int Sempahore::getCount() {
+	return count;
+}
--- input_driver.h
+++ input_driver.h
@@ -1,38 +1,38 @@
-///*
-// * input_driver.h
-// *
-// *  Created on: Oct 1, 2024
-// *      Author: eegge
-// */
-//
-//#include "stm32l4xx_hal.h"
-//#include "Queue.h"
-//#include "application_layer.h"
-//
-//class InputDriver
-//{
-//private:
-//    Queue* queue;
-//    int counter;
-//    Semaphore* semaphore;
-//    int pinA;
-//    int pinB;
-//    int lastB;
-//    int lastButtonState;
-//
-//    void handleButtonPress();
-//
-//
-//public:
-//
-//    InputDriver(Queue* q, Semaphore* sem, int pinA, int pinB, int pinButton);
-//
-//    void update();
-//
-//    void incrementCounter();
-//    void decrementCounter();
-//
-//    void getCounter(int *counterValue);
-//};
-//
-//#endif /* GIT_PROJECTB_INPUT_DRIVER_H_ */
+/*
+ * input_driver.h
+ *
+ *  Created on: Oct 1, 2024
+ *      Author: eegge
+ */
+
+#include "stm32l4xx_hal.h"
+#include "Queue.h"
+#include "application_layer.h"
+
+class InputDriver
+{
+private:
+    Queue* queue;
+    int counter;
+    Semaphore* semaphore;
+    int pinA;
+    int pinB;
+    int lastB;
+    int lastButtonState;
+
+    void handleButtonPress();
+
+
+public:
+
+    InputDriver(Queue* q, Semaphore* sem, int pinA, int pinB, int pinButton);
+
+    void update();
+
+    void incrementCounter();
+    void decrementCounter();
+
+    void getCounter(int *counterValue);
+};
+
+#endif /* GIT_PROJECTB_INPUT_DRIVER_H_ */
